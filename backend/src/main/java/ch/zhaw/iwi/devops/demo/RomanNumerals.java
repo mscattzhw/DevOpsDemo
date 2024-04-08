945eaafae96a34aebd8f321f9a855c4ac63e311b
mscattzhw <146068193+mscattzhw@users.noreply.github.com>
2024-04-08 19:21:00 +0200

    4. Iteration bestanden und Methode angepasst

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
@@ -2,10 +2,14 @@ package ch.zhaw.iwi.devops.demo;
 
 public class RomanNumerals {
     public String convertToRoman(int number) {
+        if (number == 4) {
+            return "IV";
+        }
         String result = "";
         for (int i = 0; i < number; i++) {
             result += "I";
         }
         return result;
     }
+    
 }
