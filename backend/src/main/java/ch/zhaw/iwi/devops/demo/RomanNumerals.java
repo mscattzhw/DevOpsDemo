5da2d80b6b58c0450e02867d2460929f8e5810f4
mscattzhw <146068193+mscattzhw@users.noreply.github.com>
2024-04-08 19:40:59 +0200

    10. Iteration alle tests bestanden

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
@@ -2,24 +2,7 @@ package ch.zhaw.iwi.devops.demo;
 
 public class RomanNumerals {
     public String convertToRoman(int number) {
-        if (number == 4) {
-            return "IV";
-        } else if (number == 5) {
-            return "V";
-        } else if (number == 6) {
-            return "VI";
-        } else if (number == 7) {
-            return "VII";
-        } else if (number == 8) {
-            return "VIII";
-        } else if (number == 9) {
-            return "IX";
-        }
-        String result = "";
-        for (int i = 0; i < number; i++) {
-            result += "I";
-        }
-        return result;
-    }
-    
+        String[] romanNumerals = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        return romanNumerals[number];
+    }     
 }
