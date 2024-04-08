a6c7a645e52de8d5cee35909769fa450bc5f83f8
mscattzhw <146068193+mscattzhw@users.noreply.github.com>
2024-04-08 19:27:30 +0200

    6. Iteration test bestanden

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
@@ -6,6 +6,8 @@ public class RomanNumerals {
             return "IV";
         } else if (number == 5) {
             return "V";
+        } else if (number == 6) {
+            return "VI";
         }
         String result = "";
         for (int i = 0; i < number; i++) {
